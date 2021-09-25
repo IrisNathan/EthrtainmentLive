86070c689bcf0dc8a48625903ac5d0320cc7877d
Iris Nathan <iris.nathan@g.austincc.edu>
2021-09-25 17:43:34 -0500

    IN updates to contracts

--- contracts/Ethrtainment.sol
+++ contracts/Ethrtainment.sol
@@ -7,17 +7,30 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 import "hardhat/console.sol";
 
-contract EthrtainmentLive is ReentrancyGuard, ERC1155 {
-  uint public constant regTicket;
-  uint public constant VIP;
-
+contract EthrtainmentLive is ERC1155 {  
   address payable ethrtainment;
-  uint contentCreatorStreamFee = 0.025 ether;
+  address payable event_company;
+  // hard code address to test
+  address payable content_creator;
+  uint contentCreatorStreamFee = 0.006 ether;
   uint majorEventStreamFee = 1 ether;
-  uint TICKET_PRICE;
+  uint TICKET_PRICE_EVENT = 0.02 ether;
+  uint TICKET_PRICE_VIP = 0.12 ether;
+  uint TICKET_PRICE_CCVIP = 0.008 ether;
 
+struct Events {
+  bytes16 streamName;
+  uint256 eventId;
+  uint256 totalTickets;
+  uint256 sales;
+  // address of buyer mapping to eventId and the number of NFTs in address
+  mapping(address => mapping(uint256 => uint256)) buyers;
+  bool isStreaming;
+}
 
-constructor() ERC1155("https://ethrtainment.example/api/item/{id}.json){
+Events streamEvent;
+
+constructor() {
   ethrtainment = payable(msg.sender);
-  }
+ }
 }
--- contracts/MintEvent.sol
+++ contracts/MintEvent.sol
@@ -3,16 +3,18 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MintEvent is ERC1155, Ownable {
-  uint public constant ADMISSION = 0;
-  uint public constant VIP = 1;
-  uint public constant MVP = 2;
+  uint public constant EVENT_ADMISSION = 0;
+  uint public constant VIP_TICKET = 1;
+  // Content Creator viewers that pay for special privilidges
+  uint public constant CCVIP_PATRONS = 2;
 
   constructor() ERC1155("") {
-    _mint(msg.sender, ADMISSION,  100, "");
-    _mint(msg.sender, VIP,  25, "");
-    _mint(msg.sender, MVP,  0, "");
+    _mint(msg.sender, EVENT_ADMISSION,  100, "");
+    _mint(msg.sender, VIP_TICKET,  25, "");
+    _mint(msg.sender, CCVIP_PATRONS,  0, "");
   }
 
   function mint(address account, uint id, uint amount) public onlyOwner {
