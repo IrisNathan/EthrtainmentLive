c9f4a3a27d5566ab4b589bc344e643d63ea9dc6f
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-10-01 14:42:30 -0500

    add carousel and buttom on stream component

--- src/pages/Stream/index.js
+++ src/pages/Stream/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import {  Row, Col, Container, Button } from "react-bootstrap";
+import { Row, Col, Container, Button, Carousel } from "react-bootstrap";
 import { container, rows, heading, ptag, button } from "./styles/stream";
-
 
 export default function Stream() {
   return (
@@ -9,13 +8,54 @@ export default function Stream() {
       <div style={container}>
         <Container>
           <Row style={rows}>
-            <Col style={heading}>Events Happening Now</Col>
+            <Col style={heading}>Start Streamming Now!</Col>
             <p style={ptag}>
               We have a variety of events going on all the time! Check below to see what is
               streamming now!
             </p>
           </Row>
-          
+          <Row>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="holder.js/800x400?text=First slide&bg=373940"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="holder.js/800x400?text=Second slide&bg=282c34"
+                  alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="holder.js/800x400?text=Third slide&bg=20232a"
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </Row>
+          <div>
+          <Button style={button} href="#">Start Streaming</Button>
+          </div>
         </Container>
       </div>
     </>
