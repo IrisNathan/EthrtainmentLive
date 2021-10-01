608091e97f89653065d10ce2108a29717f8ba099
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-10-01 10:36:40 -0500

    add about component, change about in nav

---
+++ src/pages/About/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { container } from "./styles/about";
+
+export default function About() {
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,2 @@
 export { default as Home } from './Home/index';
+export { default as About } from './About/index';
