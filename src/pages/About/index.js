dc054f91949a81f060e57d503d365dbff0c8f657
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-10-01 11:39:23 -0500

    add fonts to about section

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { container } from "./styles/about";
+import { container, heading, icons } from "./styles/about";
 import { Container, Row, Col } from "react-bootstrap";
 
 export default function About() {
@@ -7,13 +7,17 @@ export default function About() {
     <>
       <div style={ container }>
         <Container>
-        <Row>
-          <Col>How EthrtainmentLive works</Col>
+        <Row >
+            <Col style={heading}>How EthrtainmentLive works</Col>
+            <p>fdsfdsfdsf</p>
         </Row>
-        <Row>
-          <Col>User Experience</Col>
-          <Col>Streamer Experience</Col>
-          <Col>Decentralized Streaming Experience</Col>
+          <Row >
+            <i style={ icons} class="fas fa-laptop"></i>
+            <Col >User Experience</Col>
+            <i  style={ icons}class="fas fa-signal"></i>
+          <Col >Streamer Experience</Col>
+          <i style={ icons} class="fas fa-globe"></i>
+          <Col >Decentralized Streaming Experience</Col>
         </Row>
         </Container>
       </div>
--- src/pages/About/styles/about.js
+++ src/pages/About/styles/about.js
@@ -1,10 +1,24 @@
-// export const container = {
-//   display: 'flex',
-//   flexDirection: 'column',
-//   justifyContent: 'center',
-//   width: '100%',
-//   maxHeight: 'none',
-//   padding: '50px',
-//   margin: '0, 50px, 50px',
-//   color: '#e5e5e5'
-// };
+export const container = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  width: '100%',
+  maxHeight: 'none',
+  padding: '50px',
+  margin: '0, 50px, 50px',
+  // color: '#e5e5e5'
+};
+
+export const heading = {
+  fontSize: "40px",
+  fontWeight: "bold",
+  borderBottom: "#30989e 3px solid",
+  width: "10px",
+}
+
+export const icons = {
+  color: "#30989e ",
+  fontSize: "100px",
+  marginBottom: "50px",
+}
+
