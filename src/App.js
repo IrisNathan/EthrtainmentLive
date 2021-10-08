4af78e418acae3a50e09e3830004fe08706b6834
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-10-08 11:19:12 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Home, Viewer} from './pages';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Home, Viewer } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Switch>
-      <Home />
-      <Viewer />
-      </Switch>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/viewer">
+            <Viewer />
+          </Route>
+        </Switch>
       </Router>
-      
     </div>
   );
 }
