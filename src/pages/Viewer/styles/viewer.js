b89e5ef9d602e9b5e180b1678c4bf33d3720b566
Iris Nathan <iris.nathan@g.austincc.edu>
2021-10-10 12:18:05 -0500

    IN add styles for viewer page

---
+++ src/pages/Viewer/styles/viewer.js
@@ -0,0 +1,36 @@
+export const container = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center'
+}
+
+export const button = {
+  background: 'white',
+  margin: '30px',
+  padding: '20px',
+  border: 'none',
+  color: 'black'
+};
+
+export const btn2 = {
+  marginLeft: '695px',
+  background: 'white',
+  margin: '30px',
+  padding: '20px',
+  width: '140px',
+  border: 'none',
+  color: 'black'
+}
+
+export const btn3 = {
+  marginLeft: '695px',
+  background: 'white',
+  margin: '30px',
+  padding: '20px',
+  width: '140px',
+  border: 'none',
+  color: 'black'
+}
+
+
+
